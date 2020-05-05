2063743b547555746bf36ea517ab4e02f2355817
Connor McCormack <mccormack.connor.m@gmail.com>
2020-05-05 16:02:27 -0500

    tower of hanoi

--- bigOdrills.js
+++ bigOdrills.js
@@ -1,1 +1,32 @@
-starting point
+
+
+// 1. constant time - O(1).  For second part - linear O(n) - because it is looping through an array
+// 2. constant time - O(1)
+// 3. 
+// 4. linear because it is looping through an array
+// 5. linear because it is looping thru an array
+// 6. polynomial O(n^2) because it has a nested for loop but isn't growing
+// 7. linear
+// 8. log
+// 9. constant
+// 10. Linear, as the integer (n) increase, the number of operations grows linearly 
+// 11.
+// 12.
+// 13.
+// 14.
+
+// Tower of Hanoi
+const towerOfHanoi = function(n, source, dest, temp){
+  if(n===1){
+      printMove(source, dest);
+  }
+  else{
+     
+  }
+};
+
+
+function main(){
+  towerOfHanoi(4, 'A', 'C', 'B');
+
+main();
