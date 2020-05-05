670553e4567d1404e07579973c08091f8754335c
Connor McCormack <mccormack.connor.m@gmail.com>
2020-05-05 16:35:02 -0500

    working on iterative solutions

--- bigOdrills.js
+++ bigOdrills.js
@@ -10,23 +10,54 @@
 // 8. log
 // 9. constant
 // 10. Linear, as the integer (n) increase, the number of operations grows linearly 
-// 11.
-// 12.
-// 13.
-// 14.
 
-// Tower of Hanoi
+// 11.
+
+//towerOfHanoi
 const towerOfHanoi = function(n, source, dest, temp){
   if(n===1){
-      printMove(source, dest);
-  }
-  else{
-     
+    console.log(source + ' -> ' + dest);
+  } 
+  else {
+    towerOfHanoi(n-1,source, temp, dest);
+    console.log(source + ' -> ' + dest);
+    towerOfHanoi(n-1, temp, dest, source);
   }
 };
+  
+towerOfHanoi(4, 'A', 'C', 'B');
 
 
-function main(){
-  towerOfHanoi(4, 'A', 'C', 'B');
+//12-1
+function countSheep(num) {
+  for(let i = 0; i < num; i++) {
+    console.log('another sheep jumps over the fence');
+  }
+  console.log('All sheep jumped over the fence');
+}
 
-main();
+
+//12-2
+function powerCalculator(num, expo) {
+  let result = num;
+  if (expo < 0) {
+    return 'exponent should be >= 0';
+  }
+  if (expo === 0) {
+    return 1;
+  }
+  for(let i = 1; i < expo; i++) {
+    result *= num;
+  }
+  console.log(result);
+}
+
+//12-3
+function reverse(str) {
+  return str.split('').reverse().join('');
+}
+
+//12-4
+//12-5
+//12-6
+//12-7
